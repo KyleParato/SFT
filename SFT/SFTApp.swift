2237af61e1bee8e7131826f7026aff2d25757661
Kyle Parato <kyleparato@gmail.com>
2024-02-15 15:51:39 -0800

    Initial Commit

---
+++ SFT/SFTApp.swift
@@ -0,0 +1,20 @@
+//
+//  SFTApp.swift
+//  SFT
+//
+//  Created by Kyle Parato on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SFTApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
