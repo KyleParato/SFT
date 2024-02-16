b54e1e566847d00237bf441c42b33903d2809c47
roymasepequena <roy.masepequena@csu.fullerton.edu>
2024-02-15 16:19:40 -0800

    New commit

---
+++ SFT/Screens/HomeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreen.swift
+//  SFT
+//
+//  Created by Roy Joacquin Masepequena on 2/15/24.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
