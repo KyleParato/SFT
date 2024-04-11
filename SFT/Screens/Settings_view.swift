61d0b90a38fa33182481ca3b19780e00f6f86d8e
JovaniAntonio <jovaniantonio@csu.fullerton.edu>
2024-04-11 14:24:51 -0700

    Added Dark mode in Settings on Workout View

---
+++ SFT/Screens/Settings_view.swift
@@ -0,0 +1,18 @@
+//
+//  Settings_view.swift
+//  SFT
+//
+//  Created by csuftitan on 3/21/24.
+//
+
+import SwiftUI
+
+struct Settings_view: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Settings_view()
+}
